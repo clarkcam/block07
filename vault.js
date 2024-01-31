749aed699c34a0bd905045bff21d14a1e5991259
clarkcam <clark.cam2012@gmail.com>
2024-01-30 19:24:57 -0700

    added more comments

--- vault.js
+++ vault.js
@@ -4,12 +4,15 @@
 //calculate combination 3
 //output the combination
 
+//Output the greeting message
 alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
 
+//compute the combinations. I used a class for some extra practice
 let combination = {
     com1: 90 - 80,
     com2: 5 * 8,
     com3: 78 / 2,
 }
 
+//output the combination in a "pop up"
 alert(`The combination is ${combination.com1}  ${combination.com2}  ${combination.com3}!`);
