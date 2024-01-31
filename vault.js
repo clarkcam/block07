32b1cf0f15e80995f6b286e6bc27795f258597e8
clarkcam <clark.cam2012@gmail.com>
2024-01-30 19:08:00 -0700

    upload vault exercise to GitHub

---
+++ vault.js
@@ -0,0 +1,15 @@
+//Output a message
+//calculate combination 1 and save to a variable
+//calculate combination 2
+//calculate combination 3
+//output the combination
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+let combination = {
+    com1: 90 - 80,
+    com2: 5 * 8,
+    com3: 78 / 2,
+}
+
+alert(`The combination is ${combination.com1}  ${combination.com2}  ${combination.com3}!`);
